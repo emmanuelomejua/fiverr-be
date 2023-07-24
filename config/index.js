dfb719bf24f395e757e12e6558668df6ae00828b
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-07-24 04:55:34 +0100

    DB setup completed

---
+++ config/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+const { set, connect } = require('mongoose');
+const { log, error } = require('console')
+
+
+const connectDB = () => {
+    set('strictQuery', true)
+
+    connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    mongoose.connection.on('connected', () => {
+        log('DB connection successful')
+    })
+
+    mongoose.connection.on('error', (err) => {
+        error(`An error occured ${err.message}`)
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        log('Disconnected from DB')
+    })
+}
+
+module.exports = connectDB
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors')
 const {log, error} = require('console')
-
+const { json, urlencoded } = express
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cookieParser = require('cookie-parser')
+const connectDB = require('./config')
 
 const app = express();
+connectDB()
 
 
-const port = process.env.PORT || 4002
+//crosss origin resource
+app.use(cors())
+
+
+//use of middlewares
+app.use(json())
+app.use(urlencoded({extended: false}))
+app.use(morgan('common'))
+app.use(helmet())
+app.use(cookieParser())
+
+
+
+//Routes use here
+
+
+
+const port = process.env.PORT
 
 app.listen(port, (err) => {
     if(!err){
