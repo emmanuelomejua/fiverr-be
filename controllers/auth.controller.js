81f23a93636adad3bda4d2bc5071c1fc96c84401
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-07-27 15:43:54 +0100

    jwt auth added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
-const { hash } = require('bcrypt');
-const User = require('../models/User');
-const bcrypt = require('bcrypt')
+'use strict';
 
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 //encrypt password
 const encryptedPassword = (password) => {
@@ -15,14 +16,14 @@ const Register = async (req, res) => {
     if(!userExists){
         try {
             const newUser = await User.create({
-                password: encryptedPassword,
-                ...req.body
+                ...req.body,
+                password: encryptedPassword(password)
             })
             const user = await newUser.save()
             const { password, ...otherDetails } = user._doc
             res.status(201).json(otherDetails)
         } catch (error) {
-            res.status(500).json(error)
+            res.status(500).json(error.message)
         }
     } else {
         res.status(403).json('User already exists, please login')
@@ -34,13 +35,25 @@ const Register = async (req, res) => {
 const Login = async (req, res) => {
 
     const user = await User.findOne({email: req.body.email})
-    
+    console.log(user)
     if(user){
         try {
             const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+            console.log(isCorrect)
             if(isCorrect){
+                const token = jwt.sign({
+                    id: user._id,
+                    isSeller: user.isSeller
+                }, process.env.JWT_KEY)
                 const { password, ...otherDetails } = user._doc
-                 res.status(200).json('Login success!')
+                
+                res.cookie('accessToken', token, {
+                    httpOnly: true
+                })
+                .status(200)
+                .json(otherDetails)
+                
             } else {
                 res.status(401).send('Pls provide a valid username and password')
             }
