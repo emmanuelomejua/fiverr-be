841ae1709788493959f83962dac2d111d2608c7c
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-08-12 09:26:21 +0100

    update and create conversation deon

--- controllers/conversation.controller.js
+++ controllers/conversation.controller.js
@@ -3,8 +3,20 @@ const Converstaion = require('../models/Conversation')
 
 
 const createConversation = async (req, res) => {
+
+    const newConversation = new Converstaion({
+
+        id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
+        sellerId: req.sellerId ? req.userId : req.body.to,
+        buyerId: req.isSeller ? req.body.to : req.userId,
+        readBySeller: req.isSeller,
+        readByBuyer: !readBySeller
+    })
+
     try {
-        
+      const conversation = await newConversation.save()
+
+      res.status(200).json(conversation)
     } catch (error) {
         res.status(500).json(error.message)
     }
@@ -34,7 +46,16 @@ const getConverstaion = async (req, res) => {
 
 const updateConverstaion = async (req, res) => {
     try {
-        
+        const updated = await Converstaion.findOneAndUpdate({id: req.params.id}, {
+            $set: {
+                readBySeller: req.isSeller,
+                readByBuyer: !req.isSeller
+            }
+        }, {
+            new: true
+        })
+
+        res.status(200).json(updated)
     } catch (error) {
         res.status(500).json(error.message)
     }
